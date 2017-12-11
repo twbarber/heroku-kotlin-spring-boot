6a8d8af53be8aafe6a3d4e4f12f85284db7d3b9e
Tyler Barber <dev@twbarber.com>
2017-12-10 22:33:39 -0500

    Add Controller

--- src/main/kotlin/me/twbarber/heroku/Application.kt
+++ src/main/kotlin/me/twbarber/heroku/Application.kt
@@ -2,7 +2,9 @@ package me.twbarber.heroku
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class Application {
@@ -16,5 +18,17 @@ class Application {
 
 }
 
-@Controller
-class HelloController
+@RestController
+class HelloController {
+
+    @GetMapping("/")
+    fun hello() : String {
+        return "Hello, world!"
+    }
+
+    @GetMapping("/{name}")
+    fun helloName(@PathVariable name: String) : String {
+        return "Hello, $name!"
+    }
+
+}
